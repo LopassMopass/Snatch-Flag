158e3942fb757e7acbce6055aa41081820e658f2
LopassMopass <127187306+LopassMopass@users.noreply.github.com>
2024-05-28 21:20:19 +0200

    Second Commit
    Commiting new pages + some new features

--- check.php
+++ check.php
@@ -5,9 +5,12 @@ $request = $_SERVER['REQUEST_URI'];
 switch ($request) {
     case '':
     case '/':
-        $redirect = '/index.php';
+        $redirect = '/error.php';
         break;
     case '/login':
         $redirect = '/index.php';
         break;
+    case '/home':
+        $redirect = '/index.php';
+        break;    
 }
